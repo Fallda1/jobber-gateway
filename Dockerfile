26618418250c2e485d367b7ecbae76db92ffa879
“Haoxu” <hzhan445@uottawa.ca>
2024-11-27 19:01:18 -0500

    added seeding controllers and routes

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ COPY package*.json ./
 COPY tsconfig.json ./
 COPY .npmrc ./
 COPY src ./src
+# Pass the build-time variable
+ARG NPM_TOKEN
+
+# Configure npm to use the token
+RUN npm config set @Fallda1:registry https://npm.pkg.github.com/ && \
+    npm config set //npm.pkg.github.com/:_authToken=$NPM_TOKEN
 RUN npm install -g npm@latest
 RUN npm ci && npm run build
 
--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from '@gateway/services/auth-middleware';
 import { currentUserRoutes } from '@gateway/routes/current-user';
 // import { currentUserRoutes } from '@gateway/routes/current-user';
 // import { authMiddleware } from '@gateway/services/auth-middleware';
-// import { searchRoutes } from '@gateway/routes/search';
+import { searchRoutes } from '@gateway/routes/search';
 // import { buyerRoutes } from '@gateway/routes/buyer';
 // import { sellerRoutes } from '@gateway/routes/seller';
 // import { gigRoutes } from '@gateway/routes/gig';
@@ -18,7 +18,7 @@ const BASE_PATH = '/api/gateway/v1';
 export const appRoutes = (app: Application) => {
   app.use('', healthRoutes.routes());
   app.use(BASE_PATH, authRoutes.routes());
-  //   app.use(BASE_PATH, searchRoutes.routes());
+  app.use(BASE_PATH, searchRoutes.routes());
 
   app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
   //   app.use(BASE_PATH, authMiddleware.verifyUser, buyerRoutes.routes());
